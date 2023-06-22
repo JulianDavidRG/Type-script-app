6e2883e63b266255b38814ff23c82773dd84a7bb
JulianDavidRG <juliandavid12327@gmail.com>
2023-06-22 16:18:23 -0500

    Initial progress in the app

---
+++ src/services/diaryServices.ts
@@ -0,0 +1,7 @@
+import diaryData from './diaries.json'
+
+const diaries: Array<DiaryEntry> = diaryData as Array<DiaryEntry>
+
+export const getEntries = () => diaries
+
+export const addEntry = () => null
